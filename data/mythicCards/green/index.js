c9d36ca6e24d6371a2cd5a87f7051b19658fe62e
Leonid Khitskov <ivanovichivanivanov@protonmail.ch>
2022-08-23 20:25:53 +0900

    refactor: correct files with cards objects and import it to index.js

--- data/mythicCards/green/index.js
+++ data/mythicCards/green/index.js
@@ -1,114 +1,114 @@
-import greenCardsAssets from '../../../assets/MythicCards/green';
+// import greenCardsAssets from '../../../assets/MythicCards/green';
 
-const cardsData = [
+export const cardsData = [
   {
     id: 'green1',
-    cardFace: greenCardsAssets.green1,
+    cardFace: '../../../assets/MythicCards/green/green1.jpg',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green2',
-    cardFace: greenCardsAssets.green2,
+    cardFace: '../../../assets/MythicCards/green/green2.jpg',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green3',
-    cardFace: greenCardsAssets.green3,
+    cardFace: '../../../assets/MythicCards/green/green3.jpg',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green4',
-    cardFace: greenCardsAssets.green4,
+    cardFace: '../../../assets/MythicCards/green/green4.jpg',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green5',
-    cardFace: greenCardsAssets.green5,
+    cardFace: '../../../assets/MythicCards/green/green5.jpg',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green6',
-    cardFace: greenCardsAssets.green6,
+    cardFace: '../../../assets/MythicCards/green/green6.jpg',
     difficulty: 'hard',
     color:'green'
   },
   {
     id: 'green7',
-    cardFace: greenCardsAssets.green7,
+    cardFace: '../../../assets/MythicCards/green/green7.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green8',
-    cardFace: greenCardsAssets.green8,
+    cardFace: '../../../assets/MythicCards/green/green8.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green9',
-    cardFace: greenCardsAssets.green9,
+    cardFace: '../../../assets/MythicCards/green/green9.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green10',
-    cardFace: greenCardsAssets.green10,
+    cardFace: '../../../assets/MythicCards/green/green10.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green11',
-    cardFace: greenCardsAssets.green11,
+    cardFace: '../../../assets/MythicCards/green/green11.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green12',
-    cardFace: greenCardsAssets.green12,
+    cardFace: '../../../assets/MythicCards/green/green12.jpg',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green13',
-    cardFace: greenCardsAssets.green13,
+    cardFace: '../../../assets/MythicCards/green/green13.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green14',
-    cardFace: greenCardsAssets.green14,
+    cardFace: '../../../assets/MythicCards/green/green14.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green15',
-    cardFace: greenCardsAssets.green15,
+    cardFace: '../../../assets/MythicCards/green/green15.jpg',
     difficulty: 'normal',
     color:'green'
   },
   {
     id: 'green16',
-    cardFace: greenCardsAssets.green16,
+    cardFace: '../../../assets/MythicCards/green/green16.jpg',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green17',
-    cardFace: greenCardsAssets.green17,
+    cardFace: '../../../assets/MythicCards/green/green17.jpg',
     difficulty: 'easy',
     color:'green'
   },
   {
     id: 'green18',
-    cardFace: greenCardsAssets.green18,
+    cardFace: '../../../assets/MythicCards/green/green18.jpg',
     difficulty: 'easy',
     color:'green'
-  },
+  }
 ]
 
-export default cardsData
+// export default cardsData
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import {
   ancientsData
 } from "./data/ancients.js";
-import { difficulties } from "./data/difficulties.js";
+import {
+  difficulties
+} from "./data/difficulties.js";
+import { cardsData as blueCardsData } from "./data/mythicCards/blue/index.js";
+import { cardsData as brownCardsData } from "./data/mythicCards/brown/index.js";
+import { cardsData as greenCardsData } from "./data/mythicCards/green/index.js";
 
 const ancients = document.querySelectorAll('.ancient');
 const difficultiesWrap = document.querySelector('.difficulties-wrap');
 const difficultiesBut = document.querySelectorAll('.difficulty');
 const mixWrap = document.querySelector('.mix-wrap');
+const stagesWrap = document.querySelector('.stages-wrap');
 let currentAncient;
 let currentDifficulty;
 
@@ -26,7 +32,10 @@ ancients.forEach((el, i) => {
 
 difficultiesBut.forEach((el, i) => {
   el.addEventListener('click', () => {
-    mixWrap.classList.add('mix-active');
+    if (!el.classList.contains('difficulty-active')) {
+      stagesWrap.style.visibility = 'hidden';
+      mixWrap.classList.add('mix-active');
+    }
     currentDifficulty = difficulties[i].id;
     difficultiesBut.forEach((e, j) => {
       if (j === i) {
@@ -36,4 +45,9 @@ difficultiesBut.forEach((el, i) => {
       }
     })
   })
+})
+
+mixWrap.addEventListener('click', () => {
+  mixWrap.classList.remove('mix-active');
+  stagesWrap.style.visibility = 'visible';
 })
